84cb4ae50c19365ea7c2c8de0b0c8281b734a360
Michael Andino <mikeandino@unitec.edu>
2022-08-23 20:56:16 -0600

    Implementacion de sign in con google. (Tiene bugs)

--- App/app/src/main/java/com/example/fructiapp/LoginActivity.kt
+++ App/app/src/main/java/com/example/fructiapp/LoginActivity.kt
@@ -2,26 +2,49 @@ package com.example.fructiapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fructiapp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
+    companion object{
+        private const val RC_SIGN_IN = 120
+    }
+
      private lateinit var binding: ActivityLoginBinding
      private lateinit var firebaseAuth: FirebaseAuth
+     private lateinit var googleSignInClient: GoogleSignInClient
 
 
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
-
          binding = ActivityLoginBinding.inflate(layoutInflater)
+         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+             .requestIdToken(getString(R.string.default_web_client_id))
+             .requestEmail()
+             .build()
+
+         googleSignInClient = GoogleSignIn.getClient(this, gso)
+
          setContentView(binding.root)
 
          binding.singuptext.setOnClickListener{
              val intent = Intent(this, RegisterActivity::class.java)
              startActivity(intent);
+         }
+
+         binding.btnGoogle.setOnClickListener{
+             signIn()
          }
 
          binding.btnlogin.setOnClickListener {
@@ -31,7 +54,7 @@ class LoginActivity : AppCompatActivity() {
              if (email.isNotEmpty() && pass.isNotEmpty()) {
                      firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                          if (it.isSuccessful) {
-                             val intent = Intent(this, LoginActivity::class.java)
+                             //val intent = Intent(this, LoginActivity::class.java)
                              Toast.makeText(this, "Logged in!!", Toast.LENGTH_SHORT).show()
                          } else {
                              Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
@@ -43,4 +66,42 @@ class LoginActivity : AppCompatActivity() {
              }
          }
      }
+
+    private fun signIn(){
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful) {
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("LoginActivity", "firebaseAuthWithGoogle" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException){
+                Log.w("LoginActivity", "Google sign in failed", e)
+                }
+            } else {
+                Log.w("LoginActivity", exception.toString())
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {task ->
+                if (task.isSuccessful){
+                    Log.d("LoginActivity", "signInWithCredential:success")
+                    Toast.makeText(this, "Logged in!!", Toast.LENGTH_SHORT).show()
+                }else{
+                    Log.w("LoginActivity", "signInWithCredential:failuure", task.exception)
+                }
+            }
+    }
 }
